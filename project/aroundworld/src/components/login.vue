028d9c5e8a4bfa5cf89d2f8048ff36c08df5fbc8
sunnynono <948494360@qq.com>
2019-11-13 20:03:32 +0800

    change

--- project/aroundworld/src/components/login.vue
+++ project/aroundworld/src/components/login.vue
@@ -1,12 +1,16 @@
 <template>
     <div>
-        <el-field></el-field>
-        <el-filed></el-filed>
+        用户名:<el-input v-model="input" placeholder="请输入用户名"></el-input>
+        密码:<el-input placeholder="请输入密码" v-model="input" show-password></el-input>
         <el-button>登录</el-button>
     </div>
 </template>
 <script>
 export default {
-    
+    data(){
+        return{
+            input:""
+        }
+    }
 }
 </script>
