445698e100a070728f115bf229fae74c494af959
sunnynono <948494360@qq.com>
2019-11-14 09:53:08 +0800

    导航条模板已安置

--- project/aroundworld/src/router/index.js
+++ project/aroundworld/src/router/index.js
@@ -3,9 +3,11 @@ import VueRouter from 'vue-router'
 import region from '../views/region.vue'
 import login from '../components/login.vue'
 import insert from '../components/insert.vue'
+import header from '../components/header.vue'
 Vue.use(VueRouter)
 
 const routes = [
+  {path:'/header',component:header},
   {path:'/insert',component:insert},
   {path:'/login',component:login},
   {path:'/region',component:region},
