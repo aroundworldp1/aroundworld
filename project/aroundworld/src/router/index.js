5c49c28bd020146f63d474f5eca95fcc569a0316
aroundworldp1 <981649231@qq.com>
2019-11-26 09:23:02 +0800

    aa

--- project/aroundworld/src/router/index.js
+++ project/aroundworld/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import region from '../views/region.vue'
 import index from '../views/index.vue'
+import index1 from '../views/index1.vue'
 import spot from '../views/spot.vue'
 import userinfo from '../views/userinfo.vue'
 import article from '../views/article.vue'
@@ -9,6 +10,7 @@ import addcomment from '../views/addcomment.vue'
 import addarticle from '../views/addarticle.vue'
 import login from '../components/login.vue'
 import insert from '../components/insert.vue'
+
 
 
 
@@ -22,6 +24,7 @@ const routes = [
   {path:'/article',component:article},
   {path:'/addarticle',component:addarticle},
   {path:'/index',component:index},
+  {path:'/index1',component:index1},
   {path:'/spot',component:spot},
   {path:'/addcomment',component:addcomment},
   {path:'/userinfo',component:userinfo},
