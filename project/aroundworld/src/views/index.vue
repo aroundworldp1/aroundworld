6ed67a5b98db1cc051fe07bfbcc84e7153bbe4e2
aroundworldp1 <981649231@qq.com>
2019-12-02 17:32:22 +0800

    数据库修改

--- project/aroundworld/src/views/index.vue
+++ project/aroundworld/src/views/index.vue
@@ -1,17 +1,26 @@
 <template>
     <div class='background'>   
        <img style="width:1800px;height:900px;" :src="'http://127.0.0.1:3000/世界.png'" alt="" >
-       <div class='sanxia' @mouseenter="changec(0)" @mouseleave="changec(1)" @click='next(2)'>△
-           <img style='width:100%;' v-if="c==0" :src="'http://127.0.0.1:3000/'+list[1].rpic" alt="">
-           </div>
-       <div class='fushishan' @mouseenter="changef(0)" @mouseleave="changef(1)" @click='next(4)'>△
-           <img style='width:100%;' v-if="f==0" :src="'http://127.0.0.1:3000/'+list[3].rpic" alt="">
-       </div>
-       <div class='balidao'  @mouseenter="changeb(0)" @mouseleave="changeb(1)" @click='next(3)'>△
-           <img style='width:100%;' v-if="b==0" :src="'http://127.0.0.1:3000/'+list[2].rpic" alt="">
-       </div>
        <div class='dizhonghai' @mouseenter="changed(0)" @mouseleave="changed(1)" @click='next(1)'>△
             <img style='width:100%;' v-if="d==0" :src="'http://127.0.0.1:3000/'+list[0].rpic" alt="">
+       </div>
+      <div class='zhongguo' @mouseenter="changec(0)" @mouseleave="changec(1)" @click='next(2)'>△
+           <img style='width:100%;' v-if="c==0" :src="'http://127.0.0.1:3000/'+list[1].rpic" alt="">
+      </div> 
+      <div class='balidao'  @mouseenter="changeb(0)" @mouseleave="changeb(1)" @click='next(3)'>△
+           <img style='width:100%;' v-if="b==0" :src="'http://127.0.0.1:3000/'+list[2].rpic" alt="">
+       </div>
+        <div class='riben' @mouseenter="changef(0)" @mouseleave="changef(1)" @click='next(4)'>△
+           <img style='width:100%;' v-if="f==0" :src="'http://127.0.0.1:3000/'+list[3].rpic" alt="">
+       </div>
+       <div class='aiji' @mouseenter="changea(0)" @mouseleave="changea(1)" @click='next(5)'>△
+           <img style='width:100%;' v-if="a==0" :src="'http://127.0.0.1:3000/'+list[4].rpic" alt="">
+       </div>
+       <div class='meiguo' @mouseenter="changem(0)" @mouseleave="changem(1)" @click='next(6)'>△
+           <img style='width:100%;' v-if="m==0" :src="'http://127.0.0.1:3000/'+list[5].rpic" alt="">
+       </div>
+       <div class='baxi' @mouseenter="changex(0)" @mouseleave="changex(1)" @click='next(7)'>△
+           <img style='width:100%;' v-if="x==0" :src="'http://127.0.0.1:3000/'+list[6].rpic" alt="">
        </div>
     </div>
 </template>
@@ -27,6 +36,9 @@ export default {
            f:1,
            b:1,
            d:1,
+           a:1,
+           m:1,
+           x:1,
             showlogin:this.$route.query.showlogin,
              uname:this.$route.query.uname,
         }
@@ -40,16 +52,29 @@ export default {
             var b=this.b;b=i;this.b=b;
     },changed(i){
             var d=this.d;d=i;this.d=d;
+    },changea(i){
+            var a=this.a;a=i;this.a=a;
+    },changem(i){
+            var m=this.m;m=i;this.m=m;
+    },changex(i){
+            var x=this.x;x=i;this.x=x;
     },
+
     next(i){
         if(i==2){
-             var aera='三峡';
+             var aera='中国';
         }else if(i==4){
-            var aera='富士山';
+            var aera='日本';
         }else if(i==3){
             var aera='巴厘岛';
         }else if(i==1){
             var aera='地中海';
+        }else if(i==5){
+            var aera='埃及';
+        }else if(i==6){
+            var aera='美国';
+        }else if(i==7){
+            var aera='巴西';
         }else{
             var aera='';
         }    
@@ -80,21 +105,21 @@ export default {
         width:100%;
         background-color: #f0f0f0;
     }
-    .sanxia{
-        color:red;
-        width:80px;
-        height:80px;
-        position:absolute;
-        top:300px;
-        left:1200px;
-    }
-    .fushishan{
+    .zhongguo{
         color:red;
         width:80px;
         height:80px;
         position:absolute;
         top:330px;
-        left:1350px;
+        left:1200px;
+    }
+    .riben{
+        color:red;
+        width:80px;
+        height:80px;
+        position:absolute;
+        top:400px;
+        left:1370px;
     }
     .balidao{
         color:red;
@@ -102,7 +127,7 @@ export default {
         height:80px;
         position:absolute;
         top:500px;
-        left:1250px;
+        left:1290px;
     }
     .dizhonghai{
         color:red;
@@ -111,5 +136,29 @@ export default {
         position:absolute;
         top:250px;
         left:1000px;
+    }
+    .aiji{
+        color:red;
+        width:120px;
+        height:80px;
+        position:absolute;
+        top:470px;
+        left:930px;
+    }
+    .meiguo{
+        color:red;
+        width:120px;
+        height:80px;
+        position:absolute;
+        top:330px;
+        left:500px;
+    }
+    .baxi{
+        color:red;
+        width:120px;
+        height:80px;
+        position:absolute;
+        top:600px;
+        left:670px;
     }
 </style>
